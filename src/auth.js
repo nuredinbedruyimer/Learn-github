298cd8b80c7699481e0c3f6e1a56a60fb13ac406
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-12-06 21:57:41 +0300

    Add some change on main

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,8 @@
 // auth.js
 export function login(userName) {
-  processedName = userName.toUpperCase();
+  processedName = userName.toUpperCase().trim();
+  //  this is the comment i have added to check
+  //  wether conflict will emerge or not
+
   return processedName;
 }
