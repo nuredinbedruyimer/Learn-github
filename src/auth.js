06f8c63918dc1c62318d50576c537e79e75965d8
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-12-06 22:04:41 +0300

    add condition for returning name

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,13 @@
 // auth.js
 export function login(userName) {
   processedName = userName.toUpperCase();
-  return processedName;
+  trimmedName = processedName.trim();
+  //  this is the line after the trimming
+  for (char of processedName) {
+    if (chat) {
+      return trimmedName;
+    } else {
+      return processedName;
+    }
+  }
 }
